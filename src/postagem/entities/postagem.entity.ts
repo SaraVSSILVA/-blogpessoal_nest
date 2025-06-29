17b2c1e602280c8f0acfc9cb3e7f5a8dddd3bdb7
SaraVSSILVA <saravitoriads@outlook.com>
2025-06-29 13:55:27 -0300

    feat: Create TemaServices and TemaContoller Classes. Updates PostingService Class

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -2,9 +2,11 @@ import { IsNotEmpty } from 'class-validator';
 import {
   Column,
   Entity,
+  ManyToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { Tema } from '../../tema/entities/tema.entity';
 
 @Entity({ name: 'tb_postagens' })
 export class Postagem {
@@ -21,4 +23,11 @@ export class Postagem {
 
   @UpdateDateColumn()
   data: Date;
+
+  // O decorator @ManyToOne indica que a Classe Postagem tem uma relação de muitos para um com a Classe Tema.
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  @ManyToOne(() => Tema, (tema) => tema.postagem, {
+    onDelete: 'CASCADE',
+  })
+  tema: Tema;
 }
