68cf6e527db704d53aca90a08cef14c50b855455
SaraVSSILVA <saravitoriads@outlook.com>
2025-08-01 13:58:48 -0300

    fix:correct Theme.Module

--- src/theme/theme.module.ts
+++ src/theme/theme.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Theme } from './entities/theme.entity';
 import { ThemeService } from './services/theme.services';
+import { ThemeController } from './controllers/theme.controller';
 @Module({
   imports: [TypeOrmModule.forFeature([Theme])],
   providers: [ThemeService],
-  controllers: [],
+  controllers: [ThemeController],
   exports: [ThemeService],
 })
 export class ThemeModule {}
